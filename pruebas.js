e88f023fa007520b6bf5c1e5f50452118edc41f1
Giorgos Desktop <plokamistr@gmail.com>
2021-03-19 18:27:24 +0100

    First Steps JS

--- pruebas.js
+++ pruebas.js
@@ -1,16 +1,41 @@
-const Numbers = [1, 20, 39, 29, 10, 13];
-
-// CHECK IF SMALLER 40 WITH  .every
-
-//ES 6
-const isBelowThreshold = (currentValue) => currentValue < 40;
-
-//ES 5
-// Pote vazw onoma stin functionkai pote oxi opws edw katw function(valor)?
+// BASICS 
+//1
 /*
-const isBelowThreshold = function (valor) {
-    return valor<40;
-}
+let sayHi = () => alert("Hello!");
+sayHi();
 */
-console.log(Numbers.every(isBelowThreshold));
 
+//2
+/*
+let age = prompt("What is your age?", 18);
+let welcome = (age < 18) ?
+  () => alert('Hello') :
+  () => alert("Greetings!");
+
+welcome();
+*/
+
+// 3 TEST
+// ES5
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+}
+  
+  ask(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+  );
+
+  //ES6
+  function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  ask(
+    "Do you agree?",
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+  );
