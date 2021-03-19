b15e42bfd9c6b8ad7bbb3f3ffc5fc436b3010f16
Giorgos Desktop <plokamistr@gmail.com>
2021-03-19 21:44:52 +0100

    Base tareas HTML

--- pruebas.js
+++ pruebas.js
@@ -1,41 +1,11 @@
-// BASICS 
-//1
-/*
-let sayHi = () => alert("Hello!");
-sayHi();
-*/
+const formTask = document.querySelector("form");
+const addTask = document.querySelector("button");
+const writeTask = document.querySelector("input");
+const listTask = document.querySelector("ul");
 
-//2
-/*
-let age = prompt("What is your age?", 18);
-let welcome = (age < 18) ?
-  () => alert('Hello') :
-  () => alert("Greetings!");
 
-welcome();
-*/
 
-// 3 TEST
-// ES5
-function ask(question, yes, no) {
-    if (confirm(question)) yes()
-    else no();
-}
-  
-  ask(
-    "Do you agree?",
-    function() { alert("You agreed."); },
-    function() { alert("You canceled the execution."); }
-  );
 
-  //ES6
-  function ask(question, yes, no) {
-    if (confirm(question)) yes()
-    else no();
-  }
-  
-  ask(
-    "Do you agree?",
-    () => alert("You agreed."),
-    () => alert("You canceled the execution.")
-  );
+
+
+
