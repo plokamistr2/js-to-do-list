55629d2b6ca77d34519e10e7522ec2e95807e633
Giorgos Desktop <plokamistr@gmail.com>
2021-03-20 21:01:00 +0100

    Latest

--- pruebas.js
+++ pruebas.js
@@ -1,50 +1,73 @@
 const formTasks = document.querySelector("form");
 const writeTask = document.querySelector("input");
 const listTask = document.querySelector("ul");
-
-let itemsArray = [];
-
-localStorage.setItem('items', JSON.stringify(itemsArray))
-const data = JSON.parse(localStorage.getItem('items'))
+let pendingTask = document.getElementById("pendingtasks");
+const removeAll = document.getElementById("removeAll");
 
 
 // Document Title
 const docTitle = document.querySelector("title");
 docTitle.innerText = document.querySelector("h1").innerText;
 
-// Add/Remove Task Function
-function addTask(){
-  const task = document.createElement("li");
-  task.innerText = writeTask.value;
-  listTask.append(task);
 
-  ///Remove Task Function
+let tasksArray
+if (localStorage.getItem('Tasks Added')) {
+  tasksArray = JSON.parse(localStorage.getItem('Tasks Added'))
+} else {
+  tasksArray = [];
+}
+localStorage.setItem('Tasks Added', JSON.stringify(tasksArray));
+const tasksInfo = JSON.parse(localStorage.getItem('Tasks Added'));
+
+
+// Add/Remove Task Function
+const addRemoveTask = (taskText) => {
+  
+    const task = document.createElement("li");
+    task.innerText = taskText;
+    listTask.append(task);
+    
+
+  ///Remove Task on Click
   task.addEventListener("click",()=>{
     task.remove();
+    
+  //Remove from local storage
+
   });
 }
 
-
 // We can omit: if(input.value){addTask();} by adding required at HTML input element.
-formTasks.addEventListener("submit",(e)=>{
-  e.preventDefault();
-
-  itemsArray.push(writeTask.value)
-  localStorage.setItem('items', JSON.stringify(itemsArray))
+formTasks.addEventListener("submit",(event)=>{
+  event.preventDefault();
   
-    addTask();
-  clearInput();
-});
+  tasksArray.push(writeTask.value)
+  localStorage.setItem('Tasks Added', JSON.stringify(tasksArray))
+  
+  addRemoveTask(writeTask.value);
 
-data.forEach((item) => {
-  addTask(item);
-})
-
-// Clear input Function
-function clearInput(){
   writeTask.value="";
   writeTask.focus();
-}
+});
+
+
+// If i move it above addRemoveTask popsup : Cannot access 'addRemoveTask' before initialization
+tasksInfo.forEach((item) => {
+  addRemoveTask(item);
+});
+
+
+// Clear all tasks from LocalStorage and FrontEnd
+removeAll.addEventListener("click",()=>{
+  localStorage.clear();
+  while (listTask.firstChild) {
+    listTask.firstChild.remove();
+  }
+  tasksArray=[];
+});
+
+
+
 
 
 
