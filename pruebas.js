b6333563e1f2e583b741215bfba1395cce5ec7f1
Giorgos Desktop <plokamistr@gmail.com>
2021-03-20 17:28:07 +0100

    js

--- pruebas.js
+++ pruebas.js
@@ -1,30 +1,44 @@
 const formTasks = document.querySelector("form");
 const writeTask = document.querySelector("input");
 const listTask = document.querySelector("ul");
-let itemsArray =[];
 
-localStorage.setItem("Tasks added", JSON.stringify(itemsArray));
-const info = JSON.parse(localStorage.getItem('tasks'));
+let itemsArray = [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray))
+const data = JSON.parse(localStorage.getItem('items'))
 
 
 // Document Title
 const docTitle = document.querySelector("title");
 docTitle.innerText = document.querySelector("h1").innerText;
 
-
 // Add/Remove Task Function
-function addRemoveTask(){
-    const task = document.createElement("li");
-    task.innerText = writeTask.value;
-    listTask.append(task);
+function addTask(){
+  const task = document.createElement("li");
+  task.innerText = writeTask.value;
+  listTask.append(task);
 
-    
-    task.addEventListener("click",()=>{
-      task.remove();
-    });
-
-    
+  ///Remove Task Function
+  task.addEventListener("click",()=>{
+    task.remove();
+  });
 }
+
+
+// We can omit: if(input.value){addTask();} by adding required at HTML input element.
+formTasks.addEventListener("submit",(e)=>{
+  e.preventDefault();
+
+  itemsArray.push(writeTask.value)
+  localStorage.setItem('items', JSON.stringify(itemsArray))
+  
+    addTask();
+  clearInput();
+});
+
+data.forEach((item) => {
+  addTask(item);
+})
 
 // Clear input Function
 function clearInput(){
@@ -32,21 +46,13 @@ function clearInput(){
   writeTask.focus();
 }
 
-// We can omit: if(input.value){addTask();} by adding required at HTML input element.
-formTasks.addEventListener("submit",(e)=>{
-  e.preventDefault();
-
-  itemsArray.push(writeTask.value);
-  localStorage.setItem("Tasks added", JSON.stringify(itemsArray));
-
-  addRemoveTask();
-  clearInput();
-});
 
 
-info.forEach((item) => {
-  addRemoveTask(item);
-});
+  
+  
+
+
+
 
 
 
